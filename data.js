830e7b816055b97a10323162e0ca0548ee14fd02
Matt Harris <711mattman@gmail.com>
2021-01-31 17:10:08 -0500

    Finished portfolio

---
+++ data.js
@@ -0,0 +1,76 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default
+    {
+        
+        name :'Matthew Harris',
+        headerTagline: [
+                        'I am Matthew Harris-',
+                        'I am an aspiring web developer.'
+                        
+    ],
+        contactEmail:'mharris71195@gmail.com',
+        resume: "./resume.zip",
+        abouttext: "I am currently enrolled with Rutgers Full Stack Coding Program.  With this program I have experience using; HTML, CSS, Javascript, bootstrap, mySQL, MongoDB, and React. I also have experience working in a small team with my fellow bootcamp members in creating applications from scratch.",
+        aboutImage:'./backgpic.jpg',
+       ShowAboutImage:true,
+ 
+       projects:[
+        {
+            id: 1,
+            title: 'Tipster',
+            service: 'The first project we did',
+            imageSrc: "./design.png",
+            url: 'https://mattharris9.github.io/tipster/'
+        },
+        {
+            id: 2,
+            title: 'radical games',
+            service: 'second project',
+            imageSrc: "./radical.png",
+            url: 'https://raticles.herokuapp.com' 
+        },
+
+           {
+            id: 3,
+            title:'Password Generator', 
+             service:'First use of javascript', 
+             imageSrc:"./pass.png",
+             url:'https://mattharris9.github.io/passwordgenerator.github.io/'
+            },
+            {
+                id: 4,
+                title: 'Read me generator',
+                service: 'Node Program Development',
+                imageSrc: ".//clirmgen'rmgen.png",
+                url: 'https://github.com/MattHarris9/README-generator'
+            },
+            { 
+                id: 5,
+                title: 'Eat da burger',
+                service: 'MySQL, Node, Express, Handlebars and a ORM',
+                imageSrc: "./burger.png",
+                url: 'https://github.com/MattHarris9/Burger'
+            },
+            {
+                id: 6,
+                title: 'note taker',
+                service: 'Api implementation',
+                imageSrc: "./notes.png",
+                url: 'https://github.com/MattHarris9/Note-taker'
+           }
+
+                        
+                
+               
+        ],
+        social: [
+
+            {   name:'Github',
+                url:'https://github.com/MattHarris9'},
+            {
+                name: 'Linkedin',
+                url: 'https://www.linkedin.com/in/matthew-j-harris-a59191176/'
+            }
+
+        ]
+    }
